5afab20d0f1bdec6fa4272de9725afbe85e1367e
Billy Cheung <80034477+BillyCheung10botics@users.noreply.github.com>
2021-05-18 12:43:12 +0800

    Update build.sh
    reorder the command

--- scratch_extension/build.sh
+++ scratch_extension/build.sh
@@ -1,6 +1,24 @@
 git clone --depth 1 https://github.com/LLK/scratch-vm.git
 git clone --depth 1 --branch scratch-desktop https://github.com/LLK/scratch-gui.git
 git clone --depth 1 --branch v3.10.4 https://github.com/LLK/scratch-desktop.git
+
+git clone https://github.com/kebhr/scratch3-tello
+cp -r scratch3-tello/. ./
+rm -rf scratch3-tello/
+
+cd scratch-gui
+git clone https://github.com/champierre/tm2scratch.git
+git clone https://github.com/10botics/facemesh2scratch.git
+git clone https://github.com/champierre/handpose2scratch.git
+git clone https://github.com/champierre/posenet2scratch.git
+git clone https://github.com/BillyCheung10botics/watsonstt2scratch.git
+sh tm2scratch/install.sh
+sh facemesh2scratch/install.sh
+sh handpose2scratch/install.sh
+sh posenet2scratch/install.sh
+sh watsonstt2scratch/install.sh "https://token.10botics.com/WatsonToken.json"
+cd ../
+
 cd scratch-vm
 npm install
 npm link
@@ -14,18 +32,3 @@ npm link scratch-gui
 cd node_modules/scratch-gui
 npm link scratch-vm
 cd ../../../
-git clone https://github.com/kebhr/scratch3-tello
-cp -r scratch3-tello/. ./
-rm -rf scratch3-tello/
-
-cd scratch-gui
-git clone https://github.com/10botics/facemesh2scratch
-git clone https://github.com/champierre/tm2scratch.git
-git clone https://github.com/champierre/handpose2scratch
-git clone https://github.com/champierre/posenet2scratch.git
-git clone https://github.com/BillyCheung10botics/watsonstt2scratch.git
-./tm2scratch/install.sh
-./facemesh2scratch/install.sh
-./handpose2scratch/install.sh
-./posenet2scratch/install.sh
-./watsonstt2scratch/install.sh "https://token.10botics.com/WatsonToken.json"
